63b2f1852471d9f9a0e82449790b95d2a304e382
Kevin Solis <masterchorizo@Kevins-MacBook-Pro.local>
2023-11-12 15:28:56 -0800

    finalized css

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
+// HTML element var by IDs //
+var searchInputEl = document.getElementsByClassName("search-bar");
+var searchButtonEl = document.getElementsByClassName("search-button")
+
+// Function that retrieves the weather //
 let weather = {
-    'apiKey': "b79ca2075dc30fbde3e7044a167b6a51"
+    apiKey: "b79ca2075dc30fbde3e7044a167b6a51",
+    function: fetchWeather(city){
+        fetch("http://api.openweathermap.org/geo/1.0/direct?q="
+            + city
+            + "&limit=5&appid="
+            + this.apiKey)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+// Function that displays the weather //
+function displayWeather(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
 }
+};
